4061168119bf466c63bd3b1abb963aedaf33221e
gabrielli cruz <gabrielli.osc@gmail.com>
2023-09-09 01:30:47 -0300

    atualizando project

--- src/components/project/index.tsx
+++ src/components/project/index.tsx
@@ -1,23 +1,30 @@
-export default function Project(){
-    const projects = [{
-        title: 'Portifolio de Arte',
-        description: 'Portifolio Anna Julia ....',
-        tecnology: 'React, Bla bla',
-        repository: 'github/'
-    }, {
-        title: 'Daily planner',
-        description: 'Daily Planner baseado',
-        tecnology: 'JavaScript, Bla bla',
-        repository: 'github/'
-    }]
+import { useEffect } from "react";
 
+const projects = [{
+    title: '',
+    description: '',
+    technology: '',
+    repository: ''
+}]
+
+
+export default function Project(){
+    useEffect(() => {
+        async function getProjects(){
+          const response = await fetch('https://api.github.com/users/gabrielliosc/repos');
+          let projects = await response.json();
+          
+          return projects.visibility === 'public';
+        }
+        getProjects();
+      }, []);
     return (
         <ul>
             {projects.map((project, index) => (
                 <li key={index}>
                     <h3>{project.title}</h3>
                     <p>{project.description}</p>
-                    <p>{project.tecnology}</p>
+                    <p>{project.technology}</p>
                     <a>{project.repository}</a>
                 </li>
             ))}
