f67e7979ab5288203954573476525186d6b265ab
gabrielliosc <gabrielli.osc@gmail.com>
2024-01-24 00:32:14 -0300

    feat: Add Modal and tags to project section

--- src/components/project/index.tsx
+++ src/components/project/index.tsx
@@ -1,47 +1,96 @@
-import React, { useEffect, useState } from "react";
 import Languages from "./languages";
+import ModalBase from "./modal";
+
 import style from "./project.module.scss";
-import arrowIcon from "../../assets/img/arrow-up.svg";
 import codeIcon from '../../assets/img/code-solid.svg';
-import projectImg from '../../assets/img/profile.jpg'
+
+import dailyPlanner from '../../assets/img/daily_planner.png';
+import newsletter from '../../assets/img/newsletter.png';
+import landingPage from '../../assets/img/landing_page.png';
+
+import { v4 as uuidv4 } from "uuid"; //Gerador de uuid
 
 export default function Project() {
 
-    const [projects, setProjects] = useState<any[]>([]); //Create variable projects to get repositories projects from github API and method setProjects to set the result to the variable
-
-    useEffect(() => {
-
-        async function getProjects() {
-
-            const responseProj = await fetch('https://api.github.com/users/gabrielliosc/repos');
-            const repos = await responseProj.json();
-
-            const visibleProjects = await repos.filter((obj: any) => { return obj.topics.includes('visible') }) //Get only repositories that has the visible tag
-
-            setProjects(visibleProjects);
-
-        }
-
-        getProjects();
-    }, []);
-
+    const projects = [{
+        "id": uuidv4(),
+        "name": 'Daily Planner',
+        "demo": 'https://gabrielliosc.github.io/daily-planner/',
+        "repository": 'https://github.com/gabrielliosc/daily-planner',
+        "description": 'Projeto Front-end replicando um bloco de papel com layout de daily planner',
+        "stack": ['CSS', 'HTML', 'JavaScript'],
+        "imagem": dailyPlanner
+    },{
+        "id": uuidv4(),
+        "name": 'Página de newsletter',
+        "demo": '',
+        "repository": 'https://github.com/gabrielliosc/desafio-40',
+        "description": 'Desafio 40 do #boraCodar da Rocketseat',
+        "stack": ['CSS', 'HTML'],
+        "imagem": newsletter
+    },{
+        "id": uuidv4(),
+        "name": 'Plataforma educacional gameficada - PEG',
+        "demo": '',
+        "repository": 'https://github.com/gabrielliosc/hack-for-change',
+        "description": 'Projeto para o Hackhaton Hack for Change',
+        "stack": ['CSS', 'HTML', 'React.js'],
+        "imagem": newsletter
+    },{
+        "id": uuidv4(),
+        "name": 'Portfólio de uma artista',
+        "demo": '',
+        "repository": 'https://github.com/gabrielliosc/portifolio-app',
+        "description": 'Esse é um projeto de front-end para um portifólio da artista Naju',
+        "stack": ['CSS','SASS','HTML', 'React.js'],
+        "imagem": newsletter
+    },{
+        "id": uuidv4(),
+        "name": 'Landing page',
+        "demo": '',
+        "repository": 'https://github.com/gabrielliosc/projetos-comunidade',
+        "description": 'Esse é um projeto de front-end de uma landing page feita em conjunto na comunidade de mulheres na tecnologia',
+        "stack": ['CSS','HTML'],
+        "imagem": landingPage
+    },{
+        "id": uuidv4(),
+        "name": 'LadyRepair',
+        "demo": '',
+        "repository": 'https://github.com/gabrielliosc/mvp1-front-end',
+        "description": 'Site de divulgação de serviços realizados por mulheres, MVP para a pós-graduação de Engenharia de Software. Front-end da aplicação.',
+        "stack": ['CSS','HTML','JavaScript'],
+        "imagem": landingPage
+    },{
+        "id": uuidv4(),
+        "name": 'API do LadyRepair',
+        "demo": '',
+        "repository": 'https://github.com/gabrielliosc/mvp1-back-end',
+        "description": 'API criada para cadastro de serviços realizados por mulheres, MVP para a pós-graduação de Engenharia de Software. Back-end da aplicação.',
+        "stack": ['Python','Flask','Swagger'],
+        "imagem": landingPage
+    }]
 
 
     return (
         <section id="project" className={style.projects}>
             <h1>
-                <img src={codeIcon}></img>
+                <img src={codeIcon} alt=""/>
                 <span>Projetos</span>
-                <img src={codeIcon}></img>
+                <img src={codeIcon} alt=""/>
             </h1>
             <ul>
                 {projects.map(project => (
                     <li key={project.id}>
-                        {//<img className={style.projectImg} src={project.img_url} alt={'Imagem ' + project.name}></img>
-                        }
-                        <p>Título: {project.name} <a href={project.html_url} target="_blank" rel="noreferrer"><img src={arrowIcon} alt="" /></a></p>
-                        <p>Descrição: {project.description}</p>
-                        <p className={style.languages}><span>Linguages utilizadas:</span><Languages language={project.topics} ></Languages></p>
+                        <ModalBase key={uuidv4()} id={project.id} image={project.imagem} />
+                        <h2>{project.name} </h2>
+                        <p>{project.description}</p>
+                        <p className={style.languages}><span>Stacks:</span>
+                            <Languages languages={project.stack} />
+                        </p>
+                        <p>
+                            <a href={project.demo} target="_blank" rel="noreferrer" className={project.demo ? style.demo : style.none}> Demo</a>
+                            <a href={project.repository} target="_blank" rel="noreferrer" className={style.repository} >Repositório</a>
+                        </p>
                     </li>
                 ))}
             </ul>
--- src/components/project/languages/index.tsx
+++ src/components/project/languages/index.tsx
@@ -1,10 +1,3 @@
-import React, { useEffect, useState } from "react";
-import jsIcon from '../../../assets/img/JavaScript.jpg';
-import CSSIcon from '../../../assets/img/CSS.png';
-import HTMLIcon from '../../../assets/img/HTML.png';
-import tsIcon from '../../../assets/img/TypeScript.png';
-import reactIcon from '../../../assets/img/reactjs.png'
-import SCSSIcon from '../../../assets/img/SCSS.png';
 import style from "../project.module.scss";
 
 
@@ -13,60 +6,11 @@ interface Props {
     'language': Array<string>
 }
 
-export default function Languages({language}: {language: Array<string>}) {
-
-    const [languages, setLanguages] = useState<string[]>([]); //Create variable projects to get repositories projects from github API and method setProjects to set the result to the variable
-
-    useEffect(() => {
-
-        function filteredLang() {
-            
-            let filtrado = language.filter(removeValue)
-
-            setLanguages(filtrado)
-        }
-
-        filteredLang();
-    }, [language]);
-
-    function removeValue(value:string, index:number, topics: Array<string>) {
-            // If the value at the current array index matches the specified value (visible)
-            if (value === 'visible') {
-                // Removes the value from the original array
-                topics.splice(index, 1);
-                return false;
-                }
-            return true;
-        }
-        
-
-        function languageIcon(lang: string) {
-
-            switch (lang) {
-                case 'reactjs':
-                    return reactIcon;
-                case 'javascript':
-                    return jsIcon;
-                case 'css':
-                    return CSSIcon;
-                case 'sass':
-                    return SCSSIcon;
-                case 'typescript':
-                    return tsIcon;
-                case 'html':
-                    return HTMLIcon;
-                default:
-                    return '';
-            }
-        }
-    
+export default function Languages({languages}: {languages: Array<string>}) {    
         return (
-    
             <>                
                 {languages.map((language: string,index: any) => 
-                (<img className={style.icon} key={index}
-                src={languageIcon(language)} 
-                alt={language}></img>
+                (<span className={style.icon} key={index}>{language}</span>
                 ))} 
             </>
         ) 
