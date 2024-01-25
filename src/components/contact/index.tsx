3a425c90ceaf012eeb1070bb168849fd70723fe7
gabrielliosc <gabrielli.osc@gmail.com>
2024-01-24 23:49:13 -0300

    refactor: Update in contact section, add a download cv button

--- src/components/contact/index.tsx
+++ src/components/contact/index.tsx
@@ -1,4 +1,5 @@
 import style from './contact.module.scss';
+import cvIcon from '../../assets/img/cv-icon.svg';
 
 export default function Contact(){
     return (
@@ -15,6 +16,11 @@ export default function Contact(){
             </li>
             <li>
                 <a className={style.linkedin} target="_blank" href="https://www.linkedin.com/in/gabrielli-oliveira-cruz/">
+                </a>
+            </li>
+            <li>
+                <a className={style.cv} target="_blank" href='https://docs.google.com/document/d/1eb2I3lTRSMcUnwgF9r75s_GAxgkDvKtj/edit?usp=sharing&ouid=105115725418560324999&rtpof=true&sd=true'>
+                    <img src={cvIcon} alt="" />Baixar CV
                 </a>
             </li>
         </ul>
