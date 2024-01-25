e3586e16ec156d863ca2bdf3e78717a0355cc479
gabrielliosc <gabrielli.osc@gmail.com>
2024-01-24 23:40:11 -0300

    refactor: Update about section

--- src/components/about/index.tsx
+++ src/components/about/index.tsx
@@ -1,25 +1,35 @@
 import style from './about.module.scss';
 import Contact from '../contact';
-import profile from '../../assets/img/profile.jpg';
+import Education from '../education';
+import profile from '../../assets/img/profile.png';
 import codeIcon from '../../assets/img/code-solid.svg';
+
+import { useState } from 'react';
+import { ReactTyped } from "react-typed";
+
 
 
 export default function About() {
+
+    const [education, setEducation] = useState(false)
+
+    const myAppRef = document.getElementById('dev')
+
     return (
         <section id="about" className={style.about}>
             <h1><img src={codeIcon}></img><span>Sobre mim</span><img src={codeIcon}></img></h1>
             <div className={style.content}>
                 <div className={style.row1}>
                     <img id="profile" className={style.profile} src={profile} alt="Mulher branca com cabelo magenta"></img>
-                    <h2>Gabrielli de Oliveira</h2>
-                    <h3>Desenvolvedora Frontend</h3>
                 </div>
                 <div className={style.row2}>
-                    <p>Engenheira Cartógrafa pela UERJ, tenho interesse na área de tecnologia em especial na área de desenvolvimento web.</p>
-                    <p>Durante minha graduação desenvolvi interesse pela computação ao realizar disciplinas relacionadas ao tema. Após me formar continuei o estudo nessa área e apliquei em algumas experiências de trabalho os meus conhecimentos. Mantendo este interesse, optei por realizar uma transição de carreira para a área de frontend, ingressei na Pós-graduação de Engenharia de software da PUC-Rio para aprofundar mais meus estudos.</p>
+                <ReactTyped className={style.dev} strings={["> Desenvolvedora Frontend"]} typeSpeed={50} />
+                    <h3>Gabrielli de Oliveira</h3>
+                    <p>Engenheira em transição de carreira, apaixonada por desenvolvimento web e empoderamento de mulheres, dedicada à continua evolução cursando pós-graduação em Engenharia de Software.<button className={style.buttonAll} onClick={() => setEducation(!education)}> {education? 'Fechar formação' : 'Ver formação completa'}</button></p> 
                     <Contact></Contact>
                 </div>
             </div>
+            <Education className={education? undefined : style.none}></Education>
         </section>
     )
 }
