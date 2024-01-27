7712d04264ef4eea95368b13c0dee9cdef7afe03
gabrielliosc <gabrielli.osc@gmail.com>
2024-01-26 22:32:47 -0300

    feat: Add some new image source

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -3,8 +3,8 @@ import style from './footer.module.scss';
 export default function Footer(){
     return (
         <footer className={style.footer}>
-            <p><i>Desenvolvido por Gabrielli Oliveira | atualizado: 16/01/2024</i></p>
-            <p><i>Fontes: https://fontawesome.com/; <a href="https://www.flaticon.com/br/icones-gratis/css3" title="css3 ícones">Css3 ícones criados por popcornarts - Flaticon</a>; <a href="https://www.flaticon.com/br/icones-gratis/html" title="html ícones">Html ícones criados por Freepik - Flaticon</a></i></p>
+            <p><i>Desenvolvido por Gabrielli Oliveira | atualizado: 26/01/2024</i></p>
+            <p><i>Fontes: https://fontawesome.com/; <a target="_blank" href="https://icons8.com/icon/1349/website">Website</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>; <a href="https://www.flaticon.com/br/icones-gratis/css3" title="css3 ícones">Css3 ícones criados por popcornarts - Flaticon</a>; <a href="https://www.flaticon.com/br/icones-gratis/html" title="html ícones">Html ícones criados por Freepik - Flaticon</a>; <a target="_blank" href="https://undraw.co/">Undraw</a></i></p>
         </footer>
     )
 }
