c825494d37f1c1aed98a9c89b7bbe1db93dc9cd1
gabrielliosc <gabrielli.osc@gmail.com>
2024-01-26 22:29:09 -0300

    feat: Change in the experience section

--- src/components/experience/index.tsx
+++ src/components/experience/index.tsx
@@ -3,36 +3,31 @@ import htmlIcon from '../../assets/img/HTML.png';
 import cssIcon from '../../assets/img/CSS.png';
 import pythonIcon from '../../assets/img/python.png';
 import codeIcon from '../../assets/img/code-solid.svg';
+import terminalIcon from '../../assets/img/terminal.svg';
 
 export default function Experience(){
     const experiences = [{
         title: 'Secretaria Municipal de Assistência Social - RJ',
         id: 1,
-        role: 'Analista de Geoprocessamento Pleno (jan/21 - abril/23)',
-        description: 'Desenvolvimento de soluções inteligentes no ArcGIS Online (webmaps, webapps, dashbords, sites), ArcMap, ArcGIS Pro; Elaboração de formulários e coleta de dados através do Survey123; Elaboração de diagnóstico e análise de dados com Excel; Elaboração de sites no ArcGIS Experience Builder e ArcGis Hub',
-        
-    },{
-        title: 'Secretaria Municipal de Assistência Social - RJ',
-        id: 2,
-        role: 'Analista de Geoprocessamento Júnior (jul/20 - dez/20)',
-        description: 'Desenvolvimento de soluções inteligentes no ArcGIS Online (webmaps, webapps, dashbords, sites), ArcMap, ArcGIS Pro Elaboração de formulários e coleta de dados através do Survey123 ',
+        role: 'Analista de Geoprocessamento Júnior (jul/20 - dez/20) | Pleno (jan/21 - abril/23)',
+        description: 'Desenvolvimento de soluções inteligentes no ArcGIS Online (webmaps, webapps, dashbords, sites), ArcMap, ArcGIS Pro; Elaboração de formulários e coleta de dados através do Survey123; Elaboração de diagnóstico e análise de dados com Excel; Elaboração de sites no ArcGIS Experience Builder e ArcGis Hub'      
     }]
 
     const projects = [{
         expid: 1,
         project: 'Cálculo de indicador social',
         description: 'Desenvolvimento de um algoritmo que calcula o indicador social baseado na base de dados do CADúnico',
-        skills: `<img className=${style.icon} src=${pythonIcon}>`
+        skills: `<img src=${pythonIcon} alt="">`
     },{
         expid: 1,
         project: 'Elaboração de portal de dados',
         description: 'Desenvolvimento de um site de acesso à dados socioassistenciais utilizando a plataforma ArcGIS Hub',
-        skills: `<img className=${style.icon} src=${htmlIcon}> <img className=${style.icon} src=${cssIcon}>`
+        skills: `<img src=${htmlIcon} alt=""> <img src=${cssIcon} alt="">`
     },{
         expid: 1,
         project: 'Elaboração de site de voluntariado',
-        description: 'Desenvolvimento de um site de inscrição para voluntariado nas unidades da secretaria, utilizando a plataforma ArcGIS Hub.',
-        skills: `<img className=${style.icon} src=${htmlIcon}> <img className=${style.icon} src=${cssIcon}>`
+        description: `Desenvolvimento de um site de inscrição para voluntariado nas unidades da secretaria, utilizando a plataforma ArcGIS Hub. <a href="https://voluntario-pcrj.hub.arcgis.com/" target="_blank" style="text-decoration:none;color:#6c528b;" >Acesse a plataforma</a>`,
+        skills: `<img src=${htmlIcon} alt=""> <img src=${cssIcon} alt="">`
     }]
 
     function experienceProjects(id: number){
@@ -40,7 +35,7 @@ export default function Experience(){
         let project = '';
 
         for (let i=0; i<projs.length; i++){
-            project += `<h5>Projeto: ${projs[i].project}</h5><p>${projs[i].description}</p><p className=${style.skills}>Linguagens utilizadas: ${projs[i].skills}</p>`
+            project += `<h5><img src=${terminalIcon}/>${projs[i].project}</h5><p>${projs[i].description}</p><p>Linguagens utilizadas: ${projs[i].skills}</p>`
         }
         
         return project
@@ -54,9 +49,7 @@ export default function Experience(){
             {experiences.map((experience) => (
                 <li key={experience.id}>
                     <h4>{experience.title} <p>{experience.role}</p></h4>
-                    <h5 className={style.title}>Descrição</h5>
-                    <p> {experience.description}</p>
-                    <h5 className={style.title}>Projetos</h5>
+                    <h5 className={style.title}>Projetos desenvolvidos</h5>
                     <p dangerouslySetInnerHTML={{__html: experienceProjects(experience.id)}}></p>
                 </li>
             ))}
